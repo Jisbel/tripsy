d8c5dff577a2ed48a60ba46ee5cb48e37f1ca484
Jisbel C. Rodriguez <jisbelcarolina2906@gmail.com>
2024-03-08 00:44:02 -0400

    Creating the Home section

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,38 +1,42 @@
-// /** @type {import('tailwindcss').Config} */
+/** @type {import('tailwindcss').Config} */
+
+const colors = require("tailwindcss/colors");
+
 module.exports = {
-  content: ['./public/index.html', './src/**/*.{html,js}'],
+  content: ["./public/index.html", "./src/**/*.{html,js}"],
   theme: {
     colors: {
-      'primary':"#CC2D4A",
-      'secondary':"#8FA206",
-      'tertiary':"#61AEC9",
+      primary: "#CC2D4A",
+      secondary: "#8FA206",
+      tertiary: "#61AEC9",
+      white: "#FFFF",
     },
-    fontFamily: { 
+    fontFamily: {
       Montserrat: ["Montserrat", "sans-serif"],
     },
     extend: {
-       backgroundImage: {
-        'sanFrancisco': "url('../img/sanFrancisco.jpg')",
-			  'sanFranciscoDesktop': "url('../img/sanFranciscoDesktop.jpg')",
-			  'yosemite': "url('../img/yosemite.jpg')",
-			  'LA': "url('../img/LA.jpg')",
-			  'seattle': "url('../img/seattle.jpg')",
-			  'new_york': "url('../img/new_york.jpg')",
-			  'norway': "url('../img/norway.jpg')",
-			  'sydney': "url('../img/sydney.jpg')",
-			  'miami': "url('../img/miami.jpg')",
-			  'switzerland': "url('../img/switzerland.jpg')",
-			  'bali': "url('../img/bali.jpg')",
-			  'norway': "url('../img/norway.jpg')",
-			  'chicago': "url('../img/chicago.jpg')",
-			  'europe': "url('../img/europe.jpg')",
-			  'iceland': "url('../img/iceland.jpg')",
-      }
+      backgroundImage: {
+        sanFrancisco: "url('../img/sanFrancisco.jpg')",
+        sanFranciscoDesktop: "url('../img/sanFranciscoDesktop.jpg')",
+        yosemite: "url('../img/yosemite.jpg')",
+        LA: "url('../img/LA.jpg')",
+        seattle: "url('../img/seattle.jpg')",
+        new_york: "url('../img/new_york.jpg')",
+        norway: "url('../img/norway.jpg')",
+        sydney: "url('../img/sydney.jpg')",
+        miami: "url('../img/miami.jpg')",
+        switzerland: "url('../img/switzerland.jpg')",
+        bali: "url('../img/bali.jpg')",
+        norway: "url('../img/norway.jpg')",
+        chicago: "url('../img/chicago.jpg')",
+        europe: "url('../img/europe.jpg')",
+        iceland: "url('../img/iceland.jpg')",
+      },
     },
   },
   plugins: [
-    require('@tailwindcss/forms'),
-    require('@tailwindcss/typography'),
-    require('@tailwindcss/aspect-ratio'),
+    require("@tailwindcss/forms"),
+    require("@tailwindcss/typography"),
+    require("@tailwindcss/aspect-ratio"),
   ],
-}
+};
